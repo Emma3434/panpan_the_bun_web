460a5d2956956ff44651011ac8b04b7ca86373e0
Emma Wang <lemonmilklalala@163.com>
2023-03-18 12:49:07 -0400

    Clean up and static file server
    The current start command, based on the log, is react-scripts start, which is suitable for development but not for production. For a production build, consider using serve or another static file server.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 import HomePage from "./pages/HomePage.js";

fab84bbd3f7aed2e102ecdd7161d25cd506eb65d
Emma Wang <lemonmilklalala@163.com>
2023-03-18 10:38:55 -0400

    About organized

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,55 +1,34 @@
-// import { faIconName } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-
-// const renderContent = (content) => {
-//   if (content.type === 'paragraph') {
-//     return <p>{content.value}</p>;
-//   } else if (content.type === 'image') {
-//     return (
-//       <div>
-//         <img src={content.value} alt={content.caption} />
-//         <p>{content.caption}</p>
-//       </div>
-//     );
-//   } else if (content.type === 'icon') {
-//     const icon = content.value.toLowerCase().replace(/\s/g, '-');
-//     const faIcon = `fa${icon}`;
-//     const faIconObject = faIconName[faIcon];
-//     return (
-//       <div>
-//         <FontAwesomeIcon icon={faIconObject} />
-//         <p>{content.caption}</p>
-//       </div>
-//     );
-//   }
-// };
+import NavBar from "../components/Navbar";
 
 function About() {
-    const [aboutData, setAboutData] = useState([]);
-  
-    useEffect(() => {
-      fetch('https://panpan-server.herokuapp.com/about')
-        .then(response => response.json())
-        .then(data => setAboutData(data))
-        .catch(error => console.error(error));
-    }, []);
-  
-    return (
-      <div>
-        <h2>About</h2>
-        {aboutData.map((item, index) => (
-          <div key={index}>
-            {/* <img src={item.icon} alt={item.content} /> */}
-            <p>{item.icon}</p>
-            <p>{item.content}</p>
-            <p>{item.item}</p>
-          </div>
-        ))}
-      </div>
-    );
+  const [aboutData, setAboutData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://panpan-server.herokuapp.com/about')
+      .then(response => response.json())
+      .then(data => setAboutData(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  if (aboutData.length === 0) {
+    return <p>Loading...</p>;
   }
-  
-  export default About;
+
+  return (
+    <div>
+      <NavBar/>
+      <h2>About</h2>
+      {aboutData.map((aboutItem, index) => (
+        <div key={index}>
+          {/* <img src={aboutItem.icon} alt={aboutItem.content} /> */}
+          <p>{aboutItem.icon}</p>
+          <p>{aboutItem.content}</p>
+          <p>{aboutItem.item}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default About;
